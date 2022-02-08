56eda5ffe02d64217408182a4e0bf1a844f42b80
Ddorosh1 <doroshdmitriy@gmail.com>
2022-02-07 16:03:17 -0800

    Added custom server side and client side error handling

---
+++ client/src/app/errors/not-found/not-found.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from 'src/app/_services/account.service';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+
+}
